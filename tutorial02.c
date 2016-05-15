397b7fd7674b5fe5f3d8554961d4d87429575fa7
lanxia <ziyi.001@163.com>
2016-05-15 19:09:51 +0800

    the second changed

--- tutorial02.c
+++ tutorial02.c
@@ -1,7 +1,7 @@
 // tutorial02.c
 // A pedagogical video player that will stream through every video frame as fast as it can.
 //
-// This tutorial was written by Stephen Dranger (dranger@gmail.com).
+// This tutorial was written by robin (ziyi.001@163.com).
 //
 // Code based on FFplay, Copyright (c) 2003 Fabrice Bellard, 
 // and a tutorial by Martin Bohme (boehme@inb.uni-luebeckREMOVETHIS.de)
@@ -40,10 +40,6 @@ int main(int argc, char *argv[]) {
   //float           aspect_ratio;
 
   AVDictionary    *optionsDict = NULL;
-  struct SwsContext *sws_ctx = NULL;
-
-  //SDL_Overlay     *bmp = NULL;
-  SDL_Surface     *screen = NULL;
     
   SDL_Window    *m_window = NULL;
   SDL_Renderer *m_renderer = NULL;
@@ -89,67 +85,71 @@ int main(int argc, char *argv[]) {
   
   // Find the decoder for the video stream
   pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
-  if(pCodec==NULL) {
+  if(pCodec==NULL)
+  {
     fprintf(stderr, "Unsupported codec!\n");
     return -1; // Codec not found
   }
   
   // Open codec
-  if(avcodec_open2(pCodecCtx, pCodec, &optionsDict)<0)
+    if(avcodec_open2(pCodecCtx, pCodec, &optionsDict)<0)
     return -1; // Could not open codec
   
-  // Allocate video frame
-  pFrame=av_frame_alloc();
- AVFrame*   m_pFrameYUV = av_frame_alloc();
- uint8_t *  out_buffer = (uint8_t *)av_malloc(avpicture_get_size(PIX_FMT_YUV420P, pCodecCtx->coded_width, pCodecCtx->coded_height));
- avpicture_fill((AVPicture *)m_pFrameYUV , out_buffer, PIX_FMT_YUV420P, pCodecCtx->coded_width, pCodecCtx->coded_height);
- struct SwsContext *img_convert_ctx = sws_getContext(pCodecCtx->coded_width, pCodecCtx->coded_height,pCodecCtx->sw_pix_fmt,
+    // Allocate video frame
+    pFrame=av_frame_alloc();
+    AVFrame*   m_pFrameYUV = av_frame_alloc();
+    uint8_t *  out_buffer = (uint8_t *)av_malloc(avpicture_get_size(PIX_FMT_YUV420P, pCodecCtx->coded_width, pCodecCtx->coded_height));
+    avpicture_fill((AVPicture *)m_pFrameYUV , out_buffer, PIX_FMT_YUV420P, pCodecCtx->coded_width, pCodecCtx->coded_height);
+    struct SwsContext *img_convert_ctx = sws_getContext(pCodecCtx->coded_width, pCodecCtx->coded_height,pCodecCtx->sw_pix_fmt,
                                                      pCodecCtx->coded_width, pCodecCtx->coded_height,AV_PIX_FMT_YUV420P,
                                                      SWS_BICUBIC,
                                                      NULL, NULL, NULL);
     
-  // Make a screen to put our video
+    // Make a screen to put our video
     m_window = SDL_CreateWindow("test windows", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,pCodecCtx->coded_width, pCodecCtx->coded_height,SDL_WINDOW_SHOWN);
-    if(!m_window) {
+    if(!m_window)
+    {
         printf("SDL: could not create window - exiting:%s\n",SDL_GetError());
         return -1;
     }
     
- m_renderer = SDL_CreateRenderer(m_window, -1, 0);
- SDL_RenderClear(m_renderer);
- SDL_Texture *m_pSdlTexture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, pCodecCtx->coded_width, pCodecCtx->coded_height);
+    m_renderer = SDL_CreateRenderer(m_window, -1, 0);
+    SDL_RenderClear(m_renderer);
+    SDL_Texture *m_pSdlTexture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING,
+                                                   pCodecCtx->coded_width, pCodecCtx->coded_height);
     
     rect.x = 0;
     rect.y = 0;
     rect.w = pCodecCtx->coded_width;
     rect.h = pCodecCtx->coded_height;
    
-
+    
   // Read frames and save first five frames to disk
-  i=0;
-  while(av_read_frame(pFormatCtx, &packet)>=0) {
+  while(av_read_frame(pFormatCtx, &packet)>=0)
+  {
     // Is this a packet from the video stream?
-    if(packet.stream_index==videoStream) {
+    if(packet.stream_index==videoStream)
+    {
       // Decode video frame
-      avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, 
-			   &packet);
+      avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
       
       // Did we get a video frame?
-      if(frameFinished) {
+      if(frameFinished)
+      {
           // Convert the image into YUV format that SDL uses
-    sws_scale(img_convert_ctx, (const uint8_t* const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->coded_height,
+          sws_scale(img_convert_ctx, (const uint8_t* const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->coded_height,
                     m_pFrameYUV->data, m_pFrameYUV->linesize);
           
     
-    SDL_UpdateYUVTexture(m_pSdlTexture, &rect,
+          SDL_UpdateYUVTexture(m_pSdlTexture, &rect,
                          m_pFrameYUV->data[0], m_pFrameYUV->linesize[0],
                          m_pFrameYUV->data[1], m_pFrameYUV->linesize[1],
                          m_pFrameYUV->data[2], m_pFrameYUV->linesize[2]);
 
-   //SDL_RenderClear( m_renderer );
-   SDL_RenderCopy( m_renderer, m_pSdlTexture,  NULL, &rect);
+          //SDL_RenderClear( m_renderer );//this line seems nothing to do
+          SDL_RenderCopy( m_renderer, m_pSdlTexture,  NULL, &rect);
 
-    SDL_RenderPresent(m_renderer);
+          SDL_RenderPresent(m_renderer);
           
           SDL_Delay(20);
       }
@@ -158,19 +158,20 @@ int main(int argc, char *argv[]) {
     // Free the packet that was allocated by av_read_frame
     av_free_packet(&packet);
     SDL_PollEvent(&event);
-    switch(event.type) {
-    case SDL_QUIT:
-      SDL_Quit();
-      exit(0);
-      break;
-    default:
-      break;
+    switch(event.type)
+      {
+        case SDL_QUIT:
+          SDL_Quit();
+          exit(0);
+          break;
+        default:
+          break;
     }
 
   }
     
   sws_freeContext(img_convert_ctx);
-    av_frame_free(&m_pFrameYUV);
+  av_frame_free(&m_pFrameYUV);
     
   // Free the YUV frame
   av_free(pFrame);
@@ -181,5 +182,8 @@ int main(int argc, char *argv[]) {
   // Close the video file
   avformat_close_input(&pFormatCtx);
   
+  SDL_DestroyWindow(m_window);
+    
+    
   return 0;
 }
